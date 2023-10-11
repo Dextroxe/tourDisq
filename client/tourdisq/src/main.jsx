ec705a61b8a59908478db50212df2f8f77642f15
Dex <dashmathembram99@gmail.com>
2023-10-11 20:55:59 +0530

    adding project

---
+++ client/tourdisq/src/main.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Auth0Provider } from "@auth0/auth0-react";
+import { MantineProvider } from "@mantine/core";
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <React.StrictMode>
+    <Auth0Provider
+      domain="dev-i6n4cukc2d01oilh.us.auth0.com"
+      clientId="obeg7IYJXnsvmG2YuQpCiUWMuItCUfoK"
+      authorizationParams={{
+        redirect_uri: "https://6526b68179509c7ba6acbc30--serene-pony-e621c2.netlify.app/",
+      }}
+      audience="http://localhost:8000"
+      scope="openid profile email"
+    >
+      <MantineProvider >
+        <App />
+      </MantineProvider>
+    </Auth0Provider>
+  </React.StrictMode>
+);
