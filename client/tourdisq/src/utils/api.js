76ee8071baac9bb942880030c7d65eecd2023aa5
Dex <dashmathembram99@gmail.com>
2023-10-11 20:59:39 +0530

    added server link

--- client/tourdisq/src/utils/api.js
+++ client/tourdisq/src/utils/api.js
@@ -3,7 +3,7 @@ import dayjs from "dayjs";
 import { toast } from "react-toastify";
 
 export const api = axios.create({
-  baseURL: "https://datawala.vercel.app/api",
+  baseURL: "https://servertourdisq.vercel.app/api",
 });
 
 export const getAllproperties = async () => {
